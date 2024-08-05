1bc7f8071742dcce2eed6aefe1f12ebfe87751c0
Ksar Sx <egorvereykin2@gmail.com>
2024-08-05 21:04:49 +0300

    v1

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,7 +1,10 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class Controller {
 	
@@ -12,6 +15,36 @@ public class Controller {
 	
 	@FXML
 	private Pane titleBar;
+	
+	private double xOffset = 0;
+	private double yOffset = 0;
+	
+	/*
+	 * title bar buttons
+	 */
+	@FXML
+	private Pane btnClose;
+	
+	@FXML
+	private void onBtnCloseReleased(MouseEvent event) {
+		System.out.println("released");
+		Node source = (Node) event.getSource();
+		Stage stage = (Stage) source.getScene().getWindow();
+		stage.close();
+	}
+	
+	@FXML
+	private void onBtnFullReleased(MouseEvent event) {
+		
+	}
+	
+	@FXML
+	private void onBtnHideReleased(MouseEvent event) {
+		
+	}
+	/*
+	 * end of title bar buttons
+	 */
 	
 	
 	
