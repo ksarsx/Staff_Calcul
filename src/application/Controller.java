04fc95f41dd63c56c11e2345c10282e4123e2993
Ksar Sx <egorvereykin2@gmail.com>
2024-08-05 19:50:11 +0300

    Borders_Design

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,5 +1,18 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
 public class Controller {
+	
+	
+	/*
+	 * title bar
+	 */
+	
+	@FXML
+	private Pane titleBar;
+	
+	
 	
 }
